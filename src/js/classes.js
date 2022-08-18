6ed71b9298409d59ad62633c73b9f3180958223d
AtomicFeasT <atomic-feast@hotmail.com>
2022-08-18 20:21:33 -0300

    fix(classes.js): removed fillrect

--- src/js/classes.js
+++ src/js/classes.js
@@ -125,12 +125,12 @@ class Fighter extends Sprite {
     this.attackBox.position.y = this.position.y + this.attackBox.offset.y
 
     // * Renders Where The AttackBox Is
-    canvasContext.fillRect(
-      this.attackBox.position.x,
-      this.attackBox.position.y,
-      this.attackBox.width,
-      this.attackBox.height
-    )
+    // canvasContext.fillRect(
+    //   this.attackBox.position.x,
+    //   this.attackBox.position.y,
+    //   this.attackBox.width,
+    //   this.attackBox.height
+    // )
 
     this.position.x += this.velocity.x
     this.position.y += this.velocity.y
